920657980230dd93c052afcf0d40134e1fda6990
defrak2 <defranciscok22@gmail.com>
2024-09-11 13:37:25 -0400

    updated thought.js to have reaction subdocument in it

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,38 +1,38 @@
-const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+// const { Schema, model } = require('mongoose');
+// const dateFormat = require('../utils/dateFormat');
 
 
-const reactionSchema = new Schema (
-{
+// const reactionSchema = new Schema (
+// {
 
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    defualt: () => new Types.ObjectId(),
-  },
-  createdAt: {
-    type: Date,
-    defulat: Date.now,
-    get: (timestamp) => dateFormat(timestamp)
-  },
+//   reactionId: {
+//     type: Schema.Types.ObjectId,
+//     defualt: () => new Types.ObjectId(),
+//   },
 
-  reactionBody: {
-    type: String,
-    required: true,
-    maxlength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
 
-},
-{
-  toJSON: {
-    getters: true,
-  },
-}
-);
+//   reactionBody: {
+//     type: String,
+//     required: true,
+//     maxlength: 280,
+//   },
+//   username: {
+//     type: String,
+//     required: true,
+//   },
+//   createdAt: {
+//     type: Date,
+//     defulat: Date.now,
+//     get: (timestamp) => dateFormat(timestamp)
+//   },
+// },
+// {
+//   toJSON: {
+//     getters: true,
+//   },
+// }
+// );
 
-const Reaction = model('reaction', reactionSchema)
+// const Reaction = model('reaction', reactionSchema)
 
-module.exports = Reaction;
+// module.exports = Reaction;
